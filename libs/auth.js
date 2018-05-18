2e58c36003be353fe4eee773d6a645d59cd9e713
Nasr <iwcw4i@gmail.com>
2018-05-18 04:05:16 +0200

    auth , and db connection

---
+++ libs/auth.js
@@ -0,0 +1,15 @@
+module.exports = {
+  ensureAuthenticated: function (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    res.redirect('/auth/login')
+  },
+  ensureGuest: function (req, res, next) {
+    if (req.isAuthenticated()) {
+      res.redirect('/auth/secret')
+    } else {
+      return next()
+    }
+  }
+}
